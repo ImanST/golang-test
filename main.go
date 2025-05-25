e8934767997e3e75e7c39fa1b92f30772b4f5d40
iman sadafi tehrani <66745129+ImanST@users.noreply.github.com>
2025-05-25 18:08:33 +0330

    Update main.go v666666666

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ func main() {
 	// Hello world, the web server
 
 	helloHandler := func(w http.ResponseWriter, req *http.Request) {
-		io.WriteString(w, "Webhook Main branch json response V55555555555.\n")
+		io.WriteString(w, "Webhook Main branch json response V66666666.\n")
 	}
 
 	http.HandleFunc("/", helloHandler)
